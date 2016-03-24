5a3c68e37bd1a1faac15180364c28db8491fce97
Nepipenkoigor <igornepipenko@gmail.com>
2016-03-24 22:54:22 +0200

    feat(lesson-1) add class work

---
+++ lesson-1/class-work/scripts/menu.js
@@ -0,0 +1,32 @@
+/**
+ * Created by igor on 3/23/16.
+ */
+var menuList = [
+    { title: 'Животные', items: ['Кошки', 'Собаки'] },
+    { title: 'Рыбы', items: ['Акула', 'Клоун'] }
+];
+function generateMenu(list) {
+    var str = "<ul>";
+    for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
+        var menu = list_1[_i];
+        str += "<li><a class=\"title\">" + menu.title + "</a>";
+        str += "<ul>";
+        for (var _a = 0, _b = menu.items; _a < _b.length; _a++) {
+            var item = _b[_a];
+            str += "<li><a>" + item + "</a></li>";
+        }
+        str += "</li></ul>";
+    }
+    str += "</ul>";
+    return str;
+}
+var navMenuList = document.querySelector('.menu');
+navMenuList.innerHTML = generateMenu(menuList);
+navMenuList.onclick = function (e) {
+    var el = e.target;
+    var classList = el.classList;
+    if (classList.contains('title')) {
+        var parentLi = el.parentNode;
+        parentLi.classList.toggle('menu-open');
+    }
+};
--- lesson-1/class-work/scripts/menu.ts
+++ lesson-1/class-work/scripts/menu.ts
@@ -1,3 +1,56 @@
 /**
  * Created by igor on 3/23/16.
  */
+
+// let say = 'a bird > two cat';
+// let html = htmlEscape`<div> hellow ${say}</div>`;
+//
+// function htmlEscape(literals, ...placeholders) {
+//     let result = '';
+//     for (let i = 0; i < placeholders.length; i++) {
+//         result += literals[i];
+//         result += placeholders[i]
+//             .replace('/&/g', '&amp;')
+//             .replace('/</g', '&lt')
+//     }
+//     result += literals[literals.length - 1]
+//     return result;
+//
+// interface I {
+//     readonly x:number;
+//     readonly y:string;
+// }
+//
+// let i:I = {x: 1, y: 1}
+
+type menuList ={title:string,items:string[]}[]
+
+let menuList:menuList = [
+    {title: 'Животные', items: ['Кошки', 'Собаки']},
+    {title: 'Рыбы', items: ['Акула', 'Клоун']}
+]
+
+function generateMenu(list:menuList):string {
+    let str:string = `<ul>`;
+    for (let menu of list) {
+        str += `<li><a class="title">${menu.title}</a>`;
+        str += `<ul>`;
+        for (let item of menu.items) {
+            str += `<li><a>${item}</a></li>`
+        }
+        str += `</li></ul>`
+    }
+    str += `</ul>`;
+    return str;
+}
+
+let navMenuList:HTMLElement = document.querySelector('.menu') as HTMLElement;
+navMenuList.innerHTML = generateMenu(menuList);
+navMenuList.onclick = (e:MouseEvent)=> {
+    let el = e.target as HTMLElement;
+    let classList = el.classList;
+    if (classList.contains('title')) {
+        let parentLi = el.parentNode as HTMLElement;
+        parentLi.classList.toggle('menu-open')
+    }
+};
