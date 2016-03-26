6eee55d7f1b9093e1cf9d920d1addf353d78d1cd
vitaly <v1nt248@gmail.com>
2016-03-26 21:43:37 +0300

    готовое ДЗ

--- lesson-1/VitalyKashkel/work/js/app.ts
+++ lesson-1/VitalyKashkel/work/js/app.ts
@@ -1,12 +1,65 @@
-/**
- * Created by vitaly on 25.03.16.
- */
-
-interface Menu {
+interface MenuItem {
 	title: string;
-	subm: string;
+	items?: MenuItem[];
 }
 
-let a: Menu = {title: "Каталог", subm: "Sub"};
+let menu: MenuItem[] = [
+	{ title: 'Животные', items: [
+		{ title: 'Млекопитающие', items: [
+			{title: 'Коровы'},
+			{title: 'Ослы'},
+			{title: 'Собаки', items: [
+				{title: "Таксы"},
+				{title: "Овчарки"}
+			]},
+			{title: 'Тигры'}
+		]	},
+		{ title: 'Другие', items: [
+			{title: 'Змеи'},
+			{title: 'Птицы'},
+			{title: 'Ящерицы'},
+		]},
+	]},
+	{ title: 'Рыбы', items: [
+		{	title: 'Аквариумные', items: [
+			{title: 'Гуппи'},
+			{title: 'Скалярии'}
+		]	},
+		{ title: 'Форель', items: [
+			{title: 'Морская форель'}
+		]	},
+	]}
+];
 
-console.log(a);
+
+function generateMenu(menu: MenuItem[]): string {
+	let menuTree = `<ul>`;
+	for (let branch of menu) {
+		menuTree += ("items" in branch) ? `<li><a class="title">${branch.title}</a>` : `<li><a>${branch.title}</a>`;
+		if ("items" in branch) {
+			let subMenu: MenuItem[] = branch.items;
+			menuTree += generateMenu(subMenu);
+			menuTree += `</li>`;
+		}
+	}
+	menuTree += `</ul>`;
+
+	return menuTree;
+}
+
+function createMenu(): void {
+	let navMenuElement: HTMLElement = <HTMLElement>document.querySelector("nav");
+	navMenuElement.innerHTML = generateMenu(menu);
+
+	navMenuElement.onclick = (ev: MouseEvent)=> {
+		let el = ev.target as HTMLElement;
+		let classList = el.classList;
+		if (classList.contains('title')) {
+			let parentLi = el.parentNode as HTMLElement;
+			parentLi.classList.toggle("menu-open");
+		}
+	};
+	
+}
+
+document.addEventListener("DOMContentLoaded", createMenu);
