be610b4f9c78249e389a8754b7e3506af8a32450
Oleksiy Kashpurenko <okashpurenko@gmail.com>
2016-03-27 22:51:59 +0300

    menu any depth + check list

---
+++ lesson-1/OleksiyKashpurenko/menu.ts
@@ -0,0 +1,65 @@
+type menuItem = {title:string, items?:menuItem[]}
+type menuList = {title:string, items:menuItem[]}[]
+
+let menuList:menuList = [
+    {title: 'Животные', items: [
+        {title: 'Млекопитающие', items: [
+            {title: 'Коровы'},
+            {title: 'Ослы'},
+            {title: 'Собаки'},
+            {title: 'Тигры'}
+        ]},
+        {title: 'Другие', items: [
+            {title: 'Змеи'},
+            {title: 'Птицы'},
+            {title: 'Ящерицы'},
+        ]},
+    ]},
+    {title: 'Рыбы', items: [
+        {title: 'Аквариумные', items: [
+            {title: 'Гуппи'},
+            {title: 'Скалярии'}
+        ]},
+        {title: 'Форель', items: [
+            {title: 'Морская форель'}
+        ]},
+    ]}
+];
+
+function generateMenu(list:menuList):string {
+    let str:string = '';
+    for (let menu of list) {
+        str += generateItem(menu);
+    }
+    return `<ul>${str}</ul>`;
+}
+
+function generateItem(item:menuItem):string {
+    let itemClass = "last-item";
+    let str:string = "";
+    if (item.items && item.items.length) {
+        str += `<ul>`;
+        for (let subItem of item.items) {
+            str += generateItem(subItem);
+        }
+        str += `</ul>`;
+        itemClass = "title";
+    }
+
+    return `<li><a class="${itemClass}">${item.title}</a>${str}</li>`;
+}
+
+
+let navMenuList:HTMLElement = document.querySelector('.menu') as HTMLElement;
+navMenuList.innerHTML = generateMenu(menuList);
+navMenuList.onclick = (e:MouseEvent)=> {
+    let el = e.target as HTMLElement;
+    let classList = el.classList;
+    if (classList.contains('title')) {
+        let parentLi = el.parentNode as HTMLElement;
+        parentLi.classList.toggle('menu-open')
+    }
+    if (classList.contains('last-item')) {
+        el.classList.toggle('selected');
+    }
+};
