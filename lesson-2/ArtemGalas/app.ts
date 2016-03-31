e3ae66d051623051074a9d5374d8ef4565ee888a
artem_galas <artem.galas@gmail.com>
2016-03-31 13:23:54 +0300

    add comment with. Fixed Task 1

--- lesson-2/ArtemGalas/app.ts
+++ lesson-2/ArtemGalas/app.ts
@@ -1,18 +1,26 @@
-/* Worked only if one number equal in target */
-//TODO
-//function isInArray(target:number[], ...args:number[]):boolean {
-//  for (let i in args) {
-//    console.log(target.indexOf(args[i]));
-//    if (target.indexOf(args[i]) === -1){
-//      return false
-//    }
-//    else {
-//      return true
-//    }
-//  }
-//}
-//console.log(isInArray([1, 2],3,2));
+/**
+1)
+Написать функцию isInArray(), которая начиная со второго принимает переменное количество аргументов.
+  Возвращает true, если все аргументы, кроме первого входят в первый.
+  Первым всегда должен быть массив. */
+function isInArray(target:number[], ...args:number[]):boolean {
+ for (let i=0; i< args.length; i++) {
+   if (target.indexOf(args[i]) >= 0){
+     if ((i+1) === args.length) {
+       return true
+     }
+   }
+   else {
+     return false
+   }
+ }
+}
+console.log('isInArray = ',isInArray([1, 2], 2,1));
 
+/**
+2)
+писать функцию summator(), которая сумирует переданые ей аргументы.
+  Аргументы могут быть либо строкового либо числового типа. Количество их не ограничено **/
 function sumator(...numbers:(string|number)[]):number {
   let numbersArray:number[] = [];
   let count:number = 0;
@@ -22,8 +30,15 @@ function sumator(...numbers:(string|number)[]):number {
   }
   return count
 }
-console.log(sumator('3', '4', '6', 7, 8, 9));
+console.log('sumator =',sumator('3', 4, '6', 7, 8, 9));
 
+/**
+3)
+Написать функцию getUnique(arr), которая принимает аргументом неограниченое число аргументов,
+  и возвращает массив уникальных элементов. Аргумент не должен изменяться.
+  Порядок элементов результирующего массива должен совпадать с порядком,
+  в котором они встречаются в оригинальной структуре.
+  Специально обрабатывать значение NaN не обязательно. */
 let getUnique = (...numbers:number[]):number[] => {
   let returnedArray:number[] = [];
   for (let i = 0; i < numbers.length; i++) {
@@ -33,16 +48,23 @@ let getUnique = (...numbers:number[]):number[] => {
   }
   return returnedArray;
 };
-console.log(getUnique(1, 4, 3, 2, 4, 3));
+console.log('getUnique = ', getUnique(1, 4, 3, 2, 4, 3));
 
-//Работает только для одного слова, и у меня проблема со спец символаит
+/**
+4)
+Написать функцию котороя будет разворачивать буквы в словах предложения, но только лишь буквы
+цифры и специальные символы должны остаться на месте
+s1tar3t 2 hellow  ->  t1rat3s 2 wolleh
+s1ta$%r3t 2 hel^low  ->  t1ra$%t3s 2 wol^leh
+s1tar3t 2   low5  ->  t1rat3s 2   wol5
+ */
+
+//Работает корректно только для одного слова, и у меня проблема со спец символами. Буду рад замечаниям и поправкам
 function reverse(string:string):string {
   let returnStr:string = '';
   let a = string.match(/[\d\$\%]/g); //создаем масив из чисел и специальных символов
   let index:number[] = [];
-
-  console.log ('mas= ',a);
-
+  
   //создаем масив из индексов чисел, которые есть в массиве
   for(let i in a) {
    index.push(string.indexOf(a[i]));
@@ -60,4 +82,4 @@ function reverse(string:string):string {
   }
   return returnStr;
 }
-console.log(reverse('s1ta8r3t'));
+console.log('reverce = ', reverse('s1ta8r3t'));
